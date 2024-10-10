3829cd7354114a8e4c032b4d098f8cdf7880af77
matias scoccia <mati.scoccia@gmail.com>
2024-10-10 17:07:19 -0300

    Create main.py

---
+++ modulo_funciones/main.py
@@ -0,0 +1,12 @@
+import core as f
+def interfaz() :
+    f.menu()
+    f.pantalla1()
+    f.cargar_datos()
+    f.dieta()
+    f.ingresar_objetivo()
+    f.ingresar_dias_entrenamiento()
+    f.sugerir_recetas()
+    f.sugerir_rutinas()
+
+
